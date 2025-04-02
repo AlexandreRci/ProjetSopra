529d3e5eaa5824d5a945f0e6132d0cf849d08885
Yvan <luc50660@gmail.com>
2025-04-02 23:41:07 +0200

    [+] Add services to Singleton

--- Space_back/src/main/java/sopra/context/Singleton.java
+++ Space_back/src/main/java/sopra/context/Singleton.java
@@ -1,18 +1,24 @@
 package sopra.context;
 
-import sopra.service.CompteService;
+import sopra.service.*;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Singleton {
     private static Singleton instance = null;
-    private CompteService compteSrv = new CompteService();
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("contextJPA");
+    private final CompteService compteSrv = new CompteService();
+    private final JoueurService joueurSrv = new JoueurService();
+    private final PartieService partieSrv = new PartieService();
+    private final EspeceService especeSrv = new EspeceService();
+    private final PlaneteService planeteSrv = new PlaneteService();
+    private final PlanetSeedService planetSeedSrv = new PlanetSeedService();
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("contextJPA");
+    private final PossessionService possessionSrv = new PossessionService();
 
     private Singleton() {
     }
-    
+
     public static Singleton getInstance() {
         if (instance == null) {
             instance = new Singleton();
@@ -30,8 +36,32 @@ public class Singleton {
     public void closeEmf() {
         this.emf.close();
     }
-    
+
     public CompteService getCompteSrv() {
         return compteSrv;
+    }
+
+    public JoueurService getJoueurSrv() {
+        return joueurSrv;
+    }
+
+    public PartieService getPartieSrv() {
+        return partieSrv;
+    }
+
+    public EspeceService getEspeceSrv() {
+        return especeSrv;
+    }
+
+    public PlaneteService getPlaneteSrv() {
+        return planeteSrv;
+    }
+
+    public PlanetSeedService getPlanetSeedSrv() {
+        return planetSeedSrv;
+    }
+
+    public PossessionService getPossessionSrv() {
+        return possessionSrv;
     }
 }
