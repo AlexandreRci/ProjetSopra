081941d8971088f6271e642455f5104555686cc7
Yvan <luc50660@gmail.com>
2025-04-20 15:35:51 +0200

    [*]Optimize imports model

--- Space_boot/src/main/java/space/model/PlanetSeed.java
+++ Space_boot/src/main/java/space/model/PlanetSeed.java
@@ -1,17 +1,8 @@
 package space.model;
 
-import java.util.List;
+import jakarta.persistence.*;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "planet_seed")
