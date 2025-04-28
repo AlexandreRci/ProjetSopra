766bb38001cb359c177b83a5e38bf3b87b3c0f28
Alexandre Ricci <ricci.erdnaxela@gmail.com>
2025-04-28 17:36:10 +0200

    renvoie un message erreur si identifiant identique

--- Space_boot/src/main/java/space/rest/CompteRestController.java
+++ Space_boot/src/main/java/space/rest/CompteRestController.java
@@ -1,15 +1,24 @@
 package space.rest;
 
+import java.util.List;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
 import space.model.Compte;
 import space.rest.request.CompteRequest;
 import space.rest.response.CompteResponse;
 import space.service.CompteService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/compte")
@@ -47,7 +56,12 @@ public class CompteRestController {
         System.out.println(compteRequest);
         Compte compte = CompteRequest.convert(compteRequest);
         compte.setPassword(this.passwordEncoder.encode(compte.getPassword()));
-        return compteService.create(compte);
+        
+        try {
+            return compteService.create(compte);
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Identifiant déjà utilisé", e);
+        }
     }
 
     @PutMapping("/{id}")
