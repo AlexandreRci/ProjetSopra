5d5d7cf7366b2f959b08a61c1c4b09800593b57f
Alexandre Ricci <ricci.erdnaxela@gmail.com>
2025-04-28 16:53:14 +0200

    ajout de validation lors de la création de compte.

--- Space_front/src/app/creation-compte/creation-compte.component.ts
+++ Space_front/src/app/creation-compte/creation-compte.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CompteService } from '../compte.service';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
-import {Compte} from '../compte';
+import { Compte } from '../compte';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-creation-compte',
@@ -9,19 +10,22 @@ import {Compte} from '../compte';
   templateUrl: './creation-compte.component.html',
   styleUrl: './creation-compte.component.css'
 })
-export class CreationCompteComponent implements OnInit{
+export class CreationCompteComponent implements OnInit {
   public compteForm!: FormGroup;
-  public name!:  FormControl;
+  public name!: FormControl;
   public username!: FormControl;
-  public password1!:  FormControl;
-  public password2!:  FormControl;
-  
+  public password1!: FormControl;
+  public password2!: FormControl;
+
+  public compteCree: boolean = false;
+
+  constructor(private service: CompteService, private formBuilder: FormBuilder, private router: Router) {}
+
   ngOnInit(): void {
     this.name = this.formBuilder.control('', Validators.required);
     this.username = this.formBuilder.control('', Validators.required);
-    this.password1 = this.formBuilder.control('', [Validators.required, Validators.minLength(8), Validators.maxLength(20)] );
-    this.password2 = this.formBuilder.control('', [Validators.required, Validators.pattern(this.password1.value)] );
-
+    this.password1 = this.formBuilder.control('', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]);
+    this.password2 = this.formBuilder.control('', [Validators.required, Validators.pattern(this.password1.value)]);
 
     this.compteForm = this.formBuilder.group({
       name: this.name,
@@ -29,34 +33,39 @@ export class CreationCompteComponent implements OnInit{
       password1: this.password1,
       password2: this.password2,
     }, { validators: passwordMatchValidator() });
-
   }
 
-  constructor(private service: CompteService, private formBuilder: FormBuilder ){}
-
-  public addCompte() {
+  public addCompte(): void {
     console.log("Création d'un compte", this.compteForm.value);
+
     this.service.save(new Compte(
       null,
       this.compteForm.value.name,
       this.compteForm.value.username,
       this.compteForm.value.password1,
-      "UTILISATEUR" 
+      "UTILISATEUR"
     )).subscribe(
       (response) => {
         console.log("Réponse du serveur", response);
         this.compteForm.reset();
+        this.compteCree = true;
+
+        setTimeout(() => {
+          this.router.navigate(['/seConnecter']);
+        }, 2000); // 2 secondes avant dêtre rediriger 
+      },
+      (error) => {
+        console.error('Erreur lors de la création du compte', error);
       }
     );
   }
-
 }
+
 function passwordMatchValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const password1 = control.get('password1')?.value;
-      const password2 = control.get('password2')?.value;
-  
-      return password1 && password2 && password1 !== password2 ? { passwordMismatch: true } : null;
-    };
-}
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password1 = control.get('password1')?.value;
+    const password2 = control.get('password2')?.value;
 
+    return password1 && password2 && password1 !== password2 ? { passwordMismatch: true } : null;
+  };
+}
