968b2a6ba3a5d3c4ff4079c8803812bd4a9fe429
Yvan <luc50660@gmail.com>
2025-04-28 15:22:14 +0200

    [*] fix typo in app component

--- Space_front/src/app/app.component.ts
+++ Space_front/src/app/app.component.ts
@@ -14,6 +14,6 @@ export class AppComponent {
   constructor (private router: Router){}
 
   ngOnInit(): void{
-    this.router.navigate(['/ecranAcceuil'])
+    this.router.navigate(['/ecranAccueil'])
   }
 }
