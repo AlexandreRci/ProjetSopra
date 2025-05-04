0961adf4fae47525cb36012753b37e199e2e5616
JudyPolka <polka.judythkael@gmail.com>
2025-05-04 22:29:44 +0200

    [+] Ajout du menu dépliant avec les infos sur les planètes

--- Space_front/src/app/ecran-jeu/ecran-jeu.component.ts
+++ Space_front/src/app/ecran-jeu/ecran-jeu.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './ecran-jeu.component.css'
 })
 export class EcranJeuComponent {
+  isAnimated = true; // Contrôle l'état de l'animation planet-display
+  expandedPlanetIndex: number | null = null; // Variable permettant de selectionner quel planete est "déplié"
+
+  togglePlanetInfo(index: number) {
+    // Fonction permettant de déplier les informations d'une planete
+    // En vrai, on peut le réduire à un ternaire mais c'est plus lisible pour moi
+    if (this.expandedPlanetIndex === index) {
+      this.expandedPlanetIndex = null;
+    } else {
+      this.expandedPlanetIndex = index;
+    }
+  }
+
+
+
 
 }
