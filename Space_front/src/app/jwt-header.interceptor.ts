1c10e06619e314de51b5977f4391f36cf673e7db
JudyPolka <polka.judythkael@gmail.com>
2025-05-14 16:53:23 +0200

    [*]  Ajout console.log pour debugger

--- Space_front/src/app/jwt-header.interceptor.ts
+++ Space_front/src/app/jwt-header.interceptor.ts
@@ -7,17 +7,21 @@ export const jwtHeaderInterceptor: HttpInterceptorFn = (req, next) => {
 
   console.log('URL appelée par interceptor:',req.url);
   
+  // Exclus certaines requêtes de l'ajout de l'en-tête d'autorisation
+  // On fait return dès maintenant car pas intéressant de récupérer le token
   if (req.url.endsWith("/connexion")){
     console.log('Skip le l autorisation pour le header pour l URL:',req.url);
     return next(req);
   }
-  
+
+  // Clone la requête et ajoute l'en-tête d'autorisation
   const authRequest = req.clone({
     setHeaders: {
       'Authorization': `Bearer ${authService.token}`
     }
   });
 
+  // Ces lignes sont doivent apparaître lorsque l'utilisateur créer une partie
   console.log('Ajouter l autorisation du header pour l URL:', req.url);
   console.log('Autorisation header', `Bearer ${authService.token}`);
 
